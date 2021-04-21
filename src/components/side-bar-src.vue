ba7c52ad4e80b9a89419c185bf3238c125183861
Tarzan <id.unreleased@gmail.com>
2021-04-22 07:00:47 +0900

    Student workflow design updated

--- src/components/side-bar-src.vue
+++ src/components/side-bar-src.vue
@@ -4,7 +4,7 @@ import { layoutComputed } from "@/state/helpers";
 
 import MetisMenu from "metismenujs/dist/metismenujs";
 
-import { menuItems } from "./student";
+import { menuItems } from "./menu";
 
 export default {
   components: {
@@ -251,6 +251,29 @@ export default {
               </ul>
             </li>
           </template>
+
+          <li>
+            <a href="javascript: void(0);" class="has-arrow waves-effect">
+              <i class="ri-share-line"></i>
+              <span>Multi Level</span>
+            </a>
+            <ul class="sub-menu" aria-expanded="true">
+              <li>
+                <a href="javascript: void(0);">Level 1.1</a>
+              </li>
+              <li>
+                <a href="javascript: void(0);" class="has-arrow">Level 1.2</a>
+                <ul class="sub-menu" aria-expanded="true">
+                  <li>
+                    <a href="javascript: void(0);">Level 2.1</a>
+                  </li>
+                  <li>
+                    <a href="javascript: void(0);">Level 2.2</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
       <!-- Sidebar -->
---
+++ src/components/student.js
@@ -0,0 +1,32 @@
+export const menuItems = [
+    {
+        id: 1,
+        label: "menuitems.menu.text",
+        isTitle: true
+    },
+    {
+        id: 2,
+        label: 'Main',
+        icon: ' ri-home-4-line',
+        link: '/'
+    },
+    {
+        id: 3,
+        label: 'Alerts',
+        icon: ' ri-notification-4-line',
+        link: '/'
+    },
+    {
+        id: 4,
+        label: 'Courses',
+        icon: 'ri-play-fill',
+        link: '/courses/0'
+    },
+       
+    {
+        id: 5,
+        label: 'Teachers',
+        icon: ' ri-user-2-fill',
+        link: '/teachers'
+    }    
+]
