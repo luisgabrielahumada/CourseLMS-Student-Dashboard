d8439262c11b35dd6c7c1bc3d4b5d6c4e8db5533
Tarzan <id.unreleased@gmail.com>
2021-04-22 23:33:55 +0900

    Buttons added to Footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,17 +1,68 @@
+<script>
+/**
+ * Cards component
+ */
+export default {
+  page: {
+    title: "Courses",
+  },
+  data() {
+    return {
+      wallet_url: this.$host + 'student/profile', 
+      mycourses_url: this.$host + 'my/courses',
+      messages_url : this.$host + 'student/message',
+      discover_url : this.$host,
+      possell_url : this.$host,
+    };
+  },
+};
+</script>
+
 <template>
   <footer class="footer">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-6">
-          2020 © Nazox.
-        </div>
-        <div class="col-sm-6">
-          <div class="text-sm-right d-none d-sm-block">
-            Crafted with
-            <i class="mdi mdi-heart text-danger"></i> by Themesdesign
-          </div>
+        <div class="col-sm-12 button-container">
+          
+          <router-link
+                to="/">
+                <b-button variant="warning">Home <i class=" ri-home-4-line align-middle ml-2"></i></b-button>
+          </router-link>
+
+          <a :href='wallet_url'>
+                <b-button variant="warning">Wallet <i class="ri-wallet-3-fill align-middle ml-2"></i></b-button>
+          </a>
+
+          <a :href='mycourses_url'>
+                <b-button variant="warning">My Courses <i class="ri-play-fill align-middle ml-2"></i></b-button>
+          </a>
+
+          <a :href='messages_url'>
+                <b-button variant="warning">Messages <i class="ri-notification-4-line align-middle ml-2"></i></b-button>
+          </a>
+
+          <a :href='discover_url'>
+                <b-button variant="warning">Discover <i class=" ri-search-2-line align-middle ml-2"></i></b-button>
+          </a>
+
+          <a :href='possell_url'>
+                <b-button variant="warning">POS Sell <i class=" ri-shopping-bag-fill align-middle ml-2"></i></b-button>
+          </a>
+
         </div>
       </div>
     </div>
   </footer>
 </template>
+
+
+<style>
+.footer{
+  height: 75px;
+}
+
+.button-container{
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
