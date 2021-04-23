cce2c9dc198b1a7a29c91ae56acf5590dda2b611
Tarzan <id.unreleased@gmail.com>
2021-04-24 03:07:16 +0900

    Footer Icon Enlarged

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -26,27 +26,27 @@ export default {
           
           <router-link
                 to="/">
-                <b-button variant="warning"><i class="ri-home-4-fill align-middle"><br>Home</i></b-button>
+                <b-button variant="warning"><i class="ri-home-4-fill align-middle icon"></i><br>Home</b-button>
           </router-link>
 
           <a :href='wallet_url'>
-                <b-button variant="warning"><i class="ri-wallet-3-fill align-middle"></i><br>Wallet</b-button>
+                <b-button variant="warning"><i class="ri-wallet-3-fill align-middle icon"></i><br>Wallet</b-button>
           </a>
 
           <a :href='mycourses_url'>
-                <b-button variant="warning"><i class="ri-play-fill align-middle"></i><br>My Courses</b-button>
+                <b-button variant="warning"><i class=" ri-play-circle-fill align-middle icon"></i><br>My Courses</b-button>
           </a>
 
           <a :href='messages_url'>
-                <b-button variant="warning"><i class="ri-notification-4-fill align-middle"></i><br>Messages</b-button>
+                <b-button variant="warning"><i class="ri-notification-4-fill align-middle icon"></i><br>Messages</b-button>
           </a>
 
           <a :href='discover_url'>
-                <b-button variant="warning"><i class=" ri-search-2-fill align-middle"></i><br>Discover</b-button>
+                <b-button variant="warning"><i class=" ri-search-2-fill align-middle icon"></i><br>Discover</b-button>
           </a>
 
           <a :href='possell_url'>
-                <b-button variant="warning"><i class=" ri-shopping-bag-fill align-middle"></i><br>POS Sell</b-button>
+                <b-button variant="warning"><i class=" ri-shopping-bag-fill align-middle icon"></i><br>POS Sell</b-button>
           </a>
 
         </div>
@@ -66,5 +66,9 @@ export default {
 .button-container{
   display: flex;
   justify-content: space-evenly;
+}
+
+.icon{
+  font-size:18px;
 }
 </style>
