299706dcde9de273f5f727ff4284630bfd829fbd
Tarzan <id.unreleased@gmail.com>
2021-05-02 03:39:53 +0900

    Student - Footer Icon Color changed

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -19,28 +19,28 @@ export default {
       <div class="row">
         <div class="col-sm-12 button-container">
           
-          <router-link to="/">
-                <b-button variant="warning"><i class="ri-home-4-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.home')}}</div></b-button>
+          <router-link to="/" class="text-center">
+                <i class="ri-home-4-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.home')}}</div>
           </router-link>
 
-          <router-link to="/wallet">
-                <b-button variant="warning"><i class="ri-wallet-3-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.wallet')}}</div></b-button>
+          <router-link to="/wallet" class="text-center">
+                <i class="ri-wallet-3-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.wallet')}}</div>
           </router-link>
 
-          <router-link to="/mycourses">
-                <b-button variant="warning"><i class=" ri-play-circle-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.mycourses')}}</div></b-button>
+          <router-link to="/mycourses" class="text-center">
+                <i class=" ri-play-circle-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.mycourses')}}</div>
           </router-link>
 
-          <router-link to="/notifications">
-                <b-button variant="warning"><i class="ri-notification-4-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.notifications')}}</div></b-button>
+          <router-link to="/notifications" class="text-center">
+                <i class="ri-notification-4-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.notifications')}}</div>
           </router-link>
 
-          <router-link to="/blogs">
-                <b-button variant="warning"><i class=" ri-search-2-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.discover')}}</div></b-button>
+          <router-link to="/blogs" class="text-center">
+                <i class=" ri-search-2-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.discover')}}</div>
           </router-link>
 
-          <router-link to="/possell">
-                <b-button variant="warning"><i class=" ri-shopping-bag-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.possell')}}</div></b-button>
+          <router-link to="/possell" class="text-center">
+                <i class=" ri-shopping-bag-fill align-middle icon"></i><div class="icon-title">{{$t('student.footer.possell')}}</div>
           </router-link>
         </div>
       </div>
